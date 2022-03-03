b0ded90d012b63877a0c0cc4a82e631e0385347c
Moein Latifi <moeinl1090@protonmail.com>
2022-03-03 20:26:31 +0330

    made some changes to data

--- app/src/main/java/ir/ariyana/whatsappui/FragmentStatus.kt
+++ app/src/main/java/ir/ariyana/whatsappui/FragmentStatus.kt
@@ -26,7 +26,7 @@ class FragmentStatus : Fragment() {
             Status("https://www.rd.com/wp-content/uploads/2017/09/01-shutterstock_476340928-Irina-Bg.jpg", "Mohadeseh", "Two Hours ago"),
             Status("https://www.business2community.com/wp-content/uploads/2014/04/profile-picture-300x300.jpg", "Ali", "20 Hours ago"),
             Status("https://www.blogads.de/account/themes/account/assets/pages/media/profile/people19.png", "David", "28 Minutes ago"),
-            Status("https://preview.redd.it/2pm4mgw45qx11.jpg?width=640&crop=smart&auto=webp&s=b1659833767350c90c8ef4fa774307be5afbe2a7", "Arthur", " ago"),
+            Status("https://preview.redd.it/2pm4mgw45qx11.jpg?width=640&crop=smart&auto=webp&s=b1659833767350c90c8ef4fa774307be5afbe2a7", "Arthur", "48 Minutes ago"),
             Status("https://preview.redd.it/l9zm6t42peu41.jpg?auto=webp&s=09073c88e5771a510b9055f46747fa2fc37a48d6", "John", "Two Hours ago"),
         )
         val recyclerAdapterStatus = RecyclerAdapterStatus(data)
